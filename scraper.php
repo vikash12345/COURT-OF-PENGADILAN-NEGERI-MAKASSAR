bf606383c80a9f69c48982f5804165a0873b7dd1
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-05 16:23:28 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -9,33 +9,20 @@ $BaseLink	=	'http://sipp.pn-makassar.go.id/list_perkara/sort/';
 	{
 		$FinalURL	=	$BaseLink . $PageLoop;
    		$Html		=	file_get_html($FinalURL);
+		$RowNumb	=	-1;
 		
 			//	Paginate all 'View' buttons
 			foreach ($Html->find("//*[@id='tablePerkaraAll']/tbody/tr") as $element) {
-					echo $No		=	$element->find('./td[1]', 0)->plaintext;
-					echo $Nomor		=	$element->find('./td[2]', 0)->plaintext;
-					echo $Tanggal		=	$element->find('./td[3]', 0)->plaintext;
-					echo $Klasifikasi	=	$element->find('./td[4]', 0)->plaintext;
-					echo $Para		=	$element->find('./td[5]', 0)->plaintext;
-					echo $Status		=	$element->find('./td[6]', 0)->plaintext;
-					echo $Lama 		=	$element->find('./td[7]', 0)->plaintext;
-					echo $Link 		=	$element->find('./td[8]/a', 0)->href;
 				
-				if($Link != null || $Link != '')
-				{
-					scraperwiki::save_sqlite(array('NO'), array('NO' => $NO , 
-					    'PageLink' => $FinalURL , 
-					    'NOMOR' => $Nomor, 
-					    'Tanggal' => $Tanggal, 
-					    'Klasifikasi' => $Klasifikasi, 
-					    'Para' => $Para, 
-					    'Status' => $Status , 
-					    'Lama' => $Lama, 
-					    'Detail_Page' => $Link));
-				}
+				$RowNumb	+=	1;
+				if ($RowNumb != 0) {
+				$Link 		=	$element->find('./td[8]/a', 0)->href;
+				$tab		=	file_get_html($Link);
+				echo $tab;
+				
 					
 
-				
+				}
 				
 					
 
