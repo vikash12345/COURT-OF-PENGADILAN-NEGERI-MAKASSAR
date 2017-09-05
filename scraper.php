b2bb864eccf7876dabc6ba32c11795c675be1b56
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-05 15:27:02 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -15,14 +15,17 @@ $BaseLink	=	'http://sipp.pn-makassar.go.id/list_perkara/sort/';
 			foreach ($Html->find("//*[@id='tablePerkaraAll']/tbody/tr") as $element) {
 			$RowNumb	+=	1;
 				if ($RowNumb != 0) {
-					echo $No		=	$element->find('./td[1]', 0);
-					echo $Nomor		=	$element->find('./td[2]', 0);
-					echo $Tanggal		=	$element->find('./td[3]', 0);
-					echo $Klasifikasi	=	$element->find('./td[4]', 0);
-					echo $Para		=	$element->find('./td[5]', 0);
-					echo $Status		=	$element->find('./td[6]', 0);
-					echo $Lama 		=	$element->find('./td[7]', 0);
-					echo $Link 		=	$element->find('./td[8]/a', 0);
+					echo $No		=	$element->find('./td[1]', 0)->plaintext;
+					echo $Nomor		=	$element->find('./td[2]', 0)->plaintext;
+					echo $Tanggal		=	$element->find('./td[3]', 0)->plaintext;
+					echo $Klasifikasi	=	$element->find('./td[4]', 0)->plaintext;
+					echo $Para		=	$element->find('./td[5]', 0)->plaintext;
+					echo $Status		=	$element->find('./td[6]', 0)->plaintext;
+					echo $Lama 		=	$element->find('./td[7]', 0)->plaintext;
+					echo $Link 		=	$element->find('./td[8]/a', 0)->src;
+					
+		scraperwiki::save_sqlite(array('NO'), array('NO' => $NO , 'NOMOR' => $NOMOR, 'Tanggal' => $Tanggal, 'Klasifikasi' => $Klasifikasi, 'Para' => $Para, 'Lama' => $Lama, 'Detail_Page' => $Link));
+
 			}
 			}
   	}
