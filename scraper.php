3c0f65233fe6fb96154e6eb7ac47316c5457e9fa
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-05 14:43:23 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -9,10 +9,17 @@ $BaseLink	=	'http://sipp.pn-makassar.go.id/list_perkara/sort/';
 	{
 		$FinalURL	=	$BaseLink . $PageLoop;
    		$Html		=	file_get_html($FinalURL);
+		$RowNumb	=	-1;
 		if ($Html) {
 			//	Paginate all 'View' buttons
 			foreach ($Html->find("//*[@id='tablePerkaraAll']/tbody") as $element) {
-			echo $element;	
+			$RowNumb	+=	1;
+				if ($RowNumb != 0) {
+					
+						$No	=	$element->find('./td[1]', 0);
+					echo $No;
+					
+				}
 			}
   	}
 	}
