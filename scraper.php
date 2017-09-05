ba20987fee2d7135ad2951efe2a41ff3008a1a58
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-05 15:15:05 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -15,7 +15,14 @@ $BaseLink	=	'http://sipp.pn-makassar.go.id/list_perkara/sort/';
 			foreach ($Html->find("//*[@id='tablePerkaraAll']/tbody/tr") as $element) {
 			$RowNumb	+=	1;
 				if ($RowNumb != 0) {
-					echo $CourtName	=	$element->find('./td[1]', 0);
+					echo $No		=	$element->find('./td[1]', 0);
+					echo $Nomor		=	$element->find('./td[2]', 0);
+					echo $Tanggal		=	$element->find('./td[3]', 0);
+					echo $Klasifikasi	=	$element->find('./td[4]', 0);
+					echo $Para		=	$element->find('./td[5]', 0);
+					echo $Status		=	$element->find('./td[6]', 0);
+					echo $Lama 		=	$element->find('./td[7]', 0);
+					echo $Link 		=	$element->find('./td[8]/a', 0);
 			}
 			}
   	}
