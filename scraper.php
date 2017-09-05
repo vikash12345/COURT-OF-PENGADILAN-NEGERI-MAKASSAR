91375a1b5003a30de3f9793e244dee847d89ebac
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-05 14:34:50 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,8 +5,10 @@ require 		'scraperwiki/simple_html_dom.php';
 $BaseLink	=	'http://sipp.pn-makassar.go.id/list_perkara/sort/';
 	
 	//	Page pagination
-	for($PageLoop = 1; $PageLoop < 2; $PageLoop++){
+	for($PageLoop = 1; $PageLoop < 2; $PageLoop++)
+	{
 		$FinalURL	=	$BaseLink . $PageLoop;
-   echo  $FinalURL;
-  }
+   		$Html		=	file_get_html($FinalURL);
+		echo $Html;
+  	}
 ?>
