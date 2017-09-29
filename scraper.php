1e8f6e99f052b8537ff7372b90e75618297cfb96
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-29 14:11:40 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -6,7 +6,7 @@ for($page = 1; $page < 3 ; $page++)
 {
 $BaseLink	=	'http://sipp.pn-makassar.go.id/list_perkara/page/'.$page;
 $html		=	file_get_html($BaseLink);
-foreach($html->find("//*[@id='tablePerkaraAll']/tbodytr") as $element)
+foreach($html->find("//*[@id='tablePerkaraAll']/tbody/tr") as $element)
 {
 	$No =	$element->find("td",0)->plaintext;
 	echo $No;
