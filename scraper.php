224d6c0504821ccf0e50276adc556bd5cc06654b
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-29 12:36:59 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,9 +2,11 @@
 
 require 		'scraperwiki.php';
 require 		'scraperwiki/simple_html_dom.php';
-$BaseLink	=	'http://sipp.pn-makassar.go.id/list_perkara/page/1';
+for($page = 1; $page < 2 ; $page++)
+{
+$BaseLink	=	'http://sipp.pn-makassar.go.id/list_perkara/page/'.$page;
 echo $html		=	file_get_html($BaseLink);	
-//$numberofpages 	=	$html->find("//*[@id='selector']/ul/li[8]/a",0)->plaintext;
+}
 	//	Page pagination
 	// $numberofpages.'<br>';
 
